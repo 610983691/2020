980958539f016873a7fd353e8759828253491ad7
tongjie <tongjie93@qq.com>
2020-06-29 17:46:48 +0800

    iochannel 学习

---
+++ codes/plan/src/main/java/com/tj/basic/io/IoTest.java
@@ -0,0 +1,37 @@
+package com.tj.basic.io;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+@Slf4j
+public class IoTest {
+
+
+    public static void main(String[] args){
+        testRead();
+    }
+    public static  void testRead(){
+
+        try {
+            ByteBuffer buf =ByteBuffer.allocate(10);
+            FileChannel fileChannel = FileChannel.open(Paths.get("C:\\Users\\tongjie\\Desktop\\test.txt"));
+            int bytesread=0;
+            while ((bytesread =fileChannel.read(buf))!=-1){
+               log.info("read bytes {} ,msg is £º{}",bytesread,new String(Arrays.copyOf(buf.array(),bytesread),"UTF-8"));
+                buf.clear();
+            }
+
+        System.out.println();
+        }catch (Exception e){
+            log.error("err,",e);
+        }
+
+
+
+    }
+}
