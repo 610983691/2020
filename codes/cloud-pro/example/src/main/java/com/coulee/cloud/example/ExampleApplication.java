9dfee9a837c7aa5364dca710624419a9745159dc
tongjie <tongjie93@qq.com>
2020-09-07 21:44:22 +0800

    example 更新

--- codes/cloud-pro/example/src/main/java/com/coulee/cloud/example/ExampleApplication.java
+++ codes/cloud-pro/example/src/main/java/com/coulee/cloud/example/ExampleApplication.java
@@ -6,15 +6,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(scanBasePackages="com.coulee.cloud.config")//必须扫描common的配置包
-@EnableFeignClients//开启feign做远程调用
-@EnableDiscoveryClient//开启服务注册与发现
-//@MapperScan("com.coulee.comsumer.dao")//
+// 由于指定的scanbasepackages，所以当前的包也需要扫描
+@SpringBootApplication(scanBasePackages = {"com.coulee.cloud.common", "com.coulee.cloud.example"}) // 必须扫描common的配置包
+@EnableFeignClients // 开启feign做远程调用
+@EnableDiscoveryClient // 开启服务注册与发现
+// @MapperScan("com.coulee.comsumer.dao")//
 @EnableTransactionManagement
 public class ExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
 
 }
